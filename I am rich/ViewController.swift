ff2468afe7885100cd7cf1e09b9d5026d6d53d8d
Blumer, Seth <Seth.Blumer@nttdata.com>
2022-06-01 13:12:21 -0400

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Blumer, Seth on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
